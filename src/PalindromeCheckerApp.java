db19b2d4906c15a3a09daf23306dca1878166411
aniviyaksivarajan2005-coder <sreenivasansri06@gmail.com>
2026-03-09 14:55:27 +0530

    UC10: encapsulated palindrpme logic in palindromeChecker app

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -2,32 +2,23 @@ import java.util.Scanner;
 
 public class PalindromeCheckerApp {
 
-    public static boolean isPalindromeRecursive(String str, int start, int end) {
-
-        if (start >= end)
-            return true;
-
-        if (str.charAt(start) != str.charAt(end))
-            return false;
-
-        return isPalindromeRecursive(str, start + 1, end - 1);
-    }
-
     public static void main(String[] args) {
 
-        System.out.println("Welcome to Palindrome Checker App");
-
         Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to Palindrome Checker App");
 
         System.out.print("Enter a word: ");
         String input = scanner.nextLine();
 
-        boolean result = isPalindromeRecursive(input, 0, input.length() - 1);
+        PalindromeChecker checker = new PalindromeChecker();
+
+        boolean result = checker.checkPalindrome(input);
 
         if (result)
-            System.out.println("Result: " + input + " is a Palindrome");
+            System.out.println(input + " is a Palindrome");
         else
-            System.out.println("Result: " + input + " is NOT a Palindrome");
+            System.out.println(input + " is NOT a Palindrome");
 
         scanner.close();
     }
